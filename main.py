490f18ab735fd1a9fb371c0efb7d96c94967a0fa
kyle-bertrand <bertrandkyle1@gmail.com>
2026-02-16 12:32:27 -0800

    Added logic, going to work on regex next

--- main.py
+++ main.py
@@ -2,50 +2,66 @@ import re
 
 #Kyle Bertrand
 #Robert Huntington
-search_list : list[str] = ["22.11","23","66.7f","123abc",
-                           "Case44","Happy","78","66.7","yes123","Book111"]
 
 patterns = [
-    ("a: integer", re.compile(r"^\d+$")),
-    ("b: float", re.compile(r"^\d+\.\d+$")),
-    ("c: float with\\d[2] after decimal", re.compile(r"^\d+\.\d{2}$")),
-    ("d: float with f", re.compile(r"^\d+\.\d+f$")),
-    ("e: Capital + lowercase + digits", re.compile(r"^[A-Z][a-z]+\d+$")),
-    ("f: Exactly 3 digits + 2 or more letters", re.compile(r"^\d{3}[A-Za-z]{2,}$"))
+    ("whitespace", re.compile(r"")),
+    ("String_literal", re.compile(r"")),
+    ("Float_literal", re.compile(r"")),
+    ("Int_literal", re.compile(r"")),
+    ("Operator", re.compile(r"")),
+    ("Seperator", re.compile(r"")),
+    ("Keyword", re.compile(r"")),
+    ("Identifier", re.compile(r""))
 ]
 
+def CutOneLineTokens(line : str) -> list[str]:
+    out = []
+    s = line
 
-def print_matches(search_list : list[str], patterns : list[str] ):
-    for s in search_list:
-        matches = []
+    while s:
+        matched = False
 
-        for desc, rx in patterns:
-                if rx.match(s):
-                    matches.append(desc)
-        if matches:
-            print(f'{s:10} -> matches:{", ".join(matches)}')
-        else:
-            print(f'{s:10} -> no matches')
+        for ttype, pat in patterns:
+            m = pat.match(s)
+            if not m:
+                continue
 
-def remove_leading_integer(text : str) -> None:
-    match = re.match(r"^(\d+)\s*", text)
+            tok = m.group(0)
 
-    if match:
-        number = match.group(1)
-        start_index = match.start(1)
-        end_index = match.end(1) -1
-        rest_of_str = text[match.end():]
+            if ttype != "whitespace": #ignore leading whitespace
+                #rename for output
+                type_map ={
+                    "String_literal" : "lit",
+                    "Float_literal": "lit",
+                    "Int_literal": "lit",
+                    "Operator": "op",
+                    "Seperator": "sep",
+                    "Keyword": "key",
+                    "Identifier": "id",
+                }
+                out.append(f"<{type_map.get(ttype,ttype)},{tok}>")
 
-        print(
-            f'Found integer {number} at the beginning of this string, '
-            f'starting at index {start_index}, ending at index {end_index}. '
-            f'The rest of the string is: {rest_of_str}. '
-        )
-    else:
-        print("Found no integer at the beginning of this string")
+            s =s[m.end():] #cut token off
+            matched = True
+            break
 
-print("\n------------Task 1 ---------------\n")
-print_matches(search_list, patterns)
-print("\n------------Task 2 ---------------\n")
-remove_leading_integer("22 street")
-remove_leading_integer("90years")
+        if not matched:
+            out.append(f"<unknown,{s[0]}>")
+            s =s[1:]
+    return out
+
+
+#test lines
+tests = [
+        "int A1=5",
+        "float BBB2 =1034.2",
+        "float cresult = A1 +BBB2 * BBB2",
+        "if (cresult >10):",
+        'print("TinyPie ")',
+]
+
+for line in tests:
+    out = CutOneLineTokens(line)
+    print(f"Test input string: {line}")
+    print(f"Output <type,token> list: {out}")
+    print()
