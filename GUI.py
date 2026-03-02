04f820796ffbaacd081a4cedd5360e3b31847f1c
kyle-bertrand <bertrandkyle1@gmail.com>
2026-03-02 11:10:28 -0800

    will continue adding the GUI elements, wrote in the ending to keep the window open -KB

--- GUI.py
+++ GUI.py
@@ -1,5 +1,5 @@
-import re
 from tkinter import *
+from tkinter import messagebox
 
 class LexerGUI:
         def __init__(self, root):
@@ -13,4 +13,27 @@ class LexerGUI:
 
                 #title
                 self.title_label = Label(self.master, text="Tinypie Lexer GUI", font=("Arial", 12, "bold"))
-                self.title_label.place(row = 0, column = 0, columnspan = 2, pady=10)
+                self.title_label.grid(row = 0, column = 0, columnspan = 2, pady=10)
+
+                #source code label
+                self.src_label = Label(self.master, text="Source Code Input:", font=("Arial", 12, "bold"))
+                self.src_label.grid(row = 1, column = 1, padx=10, sticky = W)
+
+                #output label
+                self.output_label = Label(self.master, text="Lexical Analyzed Output:", font=("Arial", 12, "bold"))
+                self.output_label.grid(row=1, column=1, padx=10, sticky=W)
+
+                #input text
+                self.src_text = Text(self.master, width=60, height=20)
+                self.src_text.grid(row=2, column=0, padx=10)
+
+                #output text
+                #handle buttons
+
+
+        #def next_line(self):
+
+root = Tk()
+app = LexerGUI(root)
+root.mainloop()
+
